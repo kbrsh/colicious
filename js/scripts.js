3bd1683a4ab6da4a8fcb3db373dd1b438c407c61
Kabir Shah <simplecooldude1@gmail.com>
2016-05-21 14:33:22 -0700

    logo

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,6 @@
 var color, previousColor;
 new Clipboard('.noStyle');
 
-var getURLParameters = function(url) {
-    var result = {};
-    var searchIndex = url.indexOf("?");
-
-    if (searchIndex == -1 ) return result;
-        var sPageURL = url.substring(searchIndex +1);
-        var sURLVariables = sPageURL.split('&');
-        for (var i = 0; i < sURLVariables.length; i++) {
-            var sParameterName = sURLVariables[i].split('=');
-            result[sParameterName[0]] = sParameterName[1];
-        }
-        return result;
-};
 
 
 function colorGen() {
