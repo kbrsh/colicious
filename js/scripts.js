36130e4376028f03c2dd679a522d0f31a69d3edf
Kabir Shah <simplecooldude1@gmail.com>
2016-05-21 15:50:13 -0700

    back

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,14 @@ function colorGen() {
   return '#' + Math.random().toString(16).slice(2, 8);
 }
 
-function updateColor() {
+function updateColor(c) {
   previousColor = color;
-  color = colorGen();
-  $('.ripple').css('background', previousColor);
+  color = c;
+  //$('.ripple').css('background', previousColor);
   $('#color').html(color);
   $('body').css('background', color);
 }
 
-function previous() {
-  $('#color').html(previousColor);
-  $('body').css('background', previousColor);
-}
 
 $('.notFooter').hide();
 document.write(`<div class='centered text-center' id='loading'><?xml version="1.0" ?><svg enable-background="new 0 0 24 24" height="50px" id="Layer_1" version="1.1" viewBox="0 0 24 24" width="50px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g fill="#FFF"><path d="M12,0C6,6,2,8.5,2,14s4.5,10,10,10s10-4.5,10-10S18,6,12,0z M12,22c-4.4,0-8-3.6-8-8s5-8,8-11c3,3,8,6.6,8,11   S16.4,22,12,22z"/><path d="M17,9l-0.7,0.7c1.1,1.1,1.8,2.6,1.8,4.2s-0.7,3.2-1.8,4.2L17,19c1.3-1.3,2-3,2-5S18.2,10.3,17,9z"/></g></svg><h1>Colicious</h1><br><p>Press Space Or Click For A New Color</p><div class='loader'></div></div>`);
@@ -34,29 +30,38 @@ $(document).ready(function() {
 }, 1000);
 
 
-  document.body.onkeyup = function(e){
+  document.onkeyup = function(e){
       if(e.keyCode == 32){
-          updateColor();
+          updateColor(colorGen());
+          $("#back").css("opacity", "1");
       }
+      e.preventDefault();
   }
 
-  $("#back").on("click", function() {
-    previous();
+  $("html").on("click", function(e) {
+    updateColor(colorGen());
+    $("#back").css("opacity", "1");
+    e.preventDefault();
   });
 
-  $("html:not(.noStyle)").on("click", function() {
-    updateColor();
+  document.getElementById("back").addEventListener("click", function(e) {
+    e.stopPropagation();
+    updateColor(previousColor);
+    $("#back").css("opacity", "0");
+    e.preventDefault();
   });
+
+
 
   $('.noStyle').on("click", function(e) {
     e.preventDefault;
   });
 
-  updateColor();
+  updateColor(colorGen());
 
 });
 
-/* Ripple Effect */
+/* Ripple Effect
 
 function _classCallCheck(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
@@ -142,4 +147,4 @@ var Ripples = function () {
     return Ripples;
 }();
 var html = document.querySelector("html")
-    var rippleHTML = new Ripples(html);
+    var rippleHTML = new Ripples(html);*/
